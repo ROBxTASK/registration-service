646bfa4e2eef401d720be62d7b2533dc779bda30
fstroh <fstroh@gmail.com>
2021-12-09 16:16:45 +0100

    fixing redirect for '/'

--- src/main/java/at/srfg/robxtask/registration/openapi/HomeController.java
+++ src/main/java/at/srfg/robxtask/registration/openapi/HomeController.java
@@ -1,9 +1,13 @@
 package at.srfg.robxtask.registration.openapi;
 
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 /**
@@ -13,9 +17,20 @@ import org.springframework.web.servlet.view.RedirectView;
 @Api(tags = {"home"})
 public class HomeController {
 
+    @Autowired
+    private final NativeWebRequest request;
+
+    @Autowired
+    public HomeController(NativeWebRequest request) {
+        this.request = request;
+    }
+
     @RequestMapping("/")
     public RedirectView index() {
-        return new RedirectView("/swagger-ui/index.html", true);
+        StringBuffer url = ((HttpServletRequest) request.getNativeRequest()).getRequestURL();
+        url.append("swagger-ui/index.html");
+        return new RedirectView(String.valueOf(url));
     }
+
 
 }
